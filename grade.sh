43107d7efecfc371c7b44fcd9ad5a60bbe332db3
metan16440425 <metan@ucsd.edu>
2023-11-07 10:51:04 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,22 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f $1]]
+then 
+    echo 'Valid files'
+else
+then 
+    echo 'Not Valid files'
+fi
+
+for file in $1
+do
+  cp file grading-area
+done
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
